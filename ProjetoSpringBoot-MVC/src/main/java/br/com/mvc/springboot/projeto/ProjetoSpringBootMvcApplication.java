6ad77f9b6bb3180043a5356acb435e916bbf82b0
Luiz Carlos <lc.filho@gmail.com>
2019-01-14 16:54:27 -0300

    Projeto Inicial. Desenvolvimento Spring Boot e MVC com Thymeleaf

---
+++ ProjetoSpringBoot-MVC/src/main/java/br/com/mvc/springboot/projeto/ProjetoSpringBootMvcApplication.java
@@ -0,0 +1,14 @@
+package br.com.mvc.springboot.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootMvcApplication.class, args);
+	}
+
+}
+
